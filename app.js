3deb8d495a21585512bab000c167b08f7c231e91
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-09-05 17:26:05 +0200

    Basic setup finished. Ready to animate.

--- app.js
+++ app.js
@@ -14,17 +14,78 @@ let colours = [
 
 const maxSize = 40;
 const minSize = 0;
+const mouseRadius = 60;
 
 // mouse position
 let mouse = {
-  x: 0,
-  y: 0,
+  x: null,
+  y: null,
 };
 
 window.addEventListener('mousemove',
   function(event) {
     mouse.x = event.x;
     mouse.y = event.y;
-    console.log('mouse');
+    console.log(mouse);
   }
 );
+
+// Constructor function
+function Particle(x, y, directionX, directionY, size, color) {
+  this.x = x;
+  this.y = y;
+  this.directionX = directionX;
+  this.directionY = directionY;
+  this.size = size;
+  this.colour = colour;
+}
+
+// Draw method
+Particle.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.size, 0, MathPi * 2, false);
+  ctx.fillStyle = this.color;
+  ctx.fill();
+}
+
+// Particle movement
+Particle.prototype.update = function() {
+  if (this.x + this.size * 2 > canvas.width || this.x - this.size * 2 < 0) {
+    this.directionX = -this.directionX
+  }
+  if (this.y - this.size * 2 < 0) {
+    this.directionY = -this.directionY
+  }
+  this.x += this.directionX;
+  this.y += this.directionY;
+
+  // Mouse interactivity
+  if (    mouse.x - this.x < mouseRadius
+      &&  mouse.x - this.x > -mouseRadius
+      &&  mouse.y - this.y < mouseRadius
+      &&  mouse.y - this.y > -mouseRadius) {
+        if(this.size < maxSize) {
+          this.size += 3;
+        }
+      } else if (this.size > minSize) {
+        this.size -= 0.1;
+      }
+      if (this.size < 0) {
+        this.size = 0;
+      }
+      this.draw();
+}
+
+function init() {
+  particleArray = [];
+  for (let i = 0; i < 1000; i++) {
+    let size = 0;
+    let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
+    let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
+    let directionX = (Math.random() * .2) - .1;
+    let directionY = (Math.random() * .2) - .1;
+    let colour = colours[Math.floor(Math.random() * colours.length)];
+    particleArray.push(new Particle(x, y, directionX, directionY, size, colour));
+  }
+}
+
